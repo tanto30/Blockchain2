ca9895d130caed4aef18a18fd5113500c1d7f6f3
Jonathan Banin <tanto.jona@gmail.com>
2019-03-23 03:14:24 +0200

    conductor
    Signed-off-by: Jonathan Banin <tanto.jona@gmail.com>

--- Conductor/conductor.py
+++ Conductor/conductor.py
@@ -8,10 +8,24 @@ from json import loads
 def find_free_port():
     s = socket()
     s.bind(('', 0))
-    return s.getsockname()[1]
-
+    port = s.getsockname()[1]
+    s.close()
+    return port
 
 processes = []
+
+
+class Process(Popen):
+    def __init__(self, port):
+        super().__init__("python ../Node " + str(port), creationflags=CREATE_NEW_CONSOLE)
+        self.port = port
+        print(f"Started node instance with PID {self.pid} on Port {port}")
+        self.address = f"http://127.0.0.1:{port}"
+
+    def send(self, ep):
+        resp = requests.get(self.address + ep)
+        print(f"PID:{self.pid}, PORT:{self.port} - {resp.status_code}")
+        return resp
 
 while (True):
     cmd = input()
@@ -22,29 +36,24 @@ while (True):
         num = int(cmd[1])
         for i in range(num):
             port = find_free_port()
-            proc = Popen("python ../Node " + str(port), creationflags=CREATE_NEW_CONSOLE)
-            proc.port = port
+            proc = Process(port)
             processes.append(proc)
-            print(f"Started node instance with PID {proc.pid} on Port {port}")
     elif cmd[0] == "stop":
         for process in processes:
             process.kill()
         processes = []
     elif cmd[0] == "mine":
         for process in processes:
-            resp = requests.get("http://127.0.0.1:" + str(process.port) + "/mine")
-            print(f"PID:{process.pid},PORT:{process.port} - {resp.status_code}")
+            process.send("/mine")
     elif cmd[0] == "register_all":
         for x in processes:
             for y in processes:
                 if x.pid != y.pid:
-                    resp = requests.get(f"http://127.0.0.1:{x.port}/register/{y.port}")
-                    print(f"PID:{x.pid},PORT:{x.port} - {resp.status_code}")
+                    x.send(f"/register/{y.port}")
     elif cmd[0] == "knowledge_graph":
         G = nx.DiGraph()
         for x in processes:
-            resp = requests.get(f"http://127.0.0.1:{x.port}/nodes")
-            print(f"PID:{x.pid},PORT:{x.port} - {resp.status_code}")
+            resp = x.send("/nodes")
             resp = loads(resp.text)
             for n in resp:
                 G.add_edge(x.port, int(n))
